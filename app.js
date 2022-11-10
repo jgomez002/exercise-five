60c950ed6a5fd4185cdb80b80823d0df01c92a28
jgomez002 <jdg8858@nyu.edu>
2022-11-10 02:41:41 -0500

    update

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+const firebase = require ("firebase/app");
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
-const homePage = require('./routes/index.js');
-const aboutPage = require('./routes/about.js');
+const firebaseConfig = {
+  apiKey: "AIzaSyD5z28T9QG1sIvMIZsw1D4ajqyqAwZjM5A",
+  authDomain: "exercise-five-dynweb.firebaseapp.com",
+  projectId: "exercise-five-dynweb",
+  storageBucket: "exercise-five-dynweb.appspot.com",
+  messagingSenderId: "729832982897",
+  appId: "1:729832982897:web:7d9f994425b4a0a8ab8ed0"
+};
 
+firebase.initializeApp(firebaseConfig);
+ 
+const indexRoute = require('./routes/index.js');
 
-app.use('/', homePage);
-app.use('/about', aboutPage);
+app.use('/', indexRoute);
 
-
-app.use('/images',express.static('public'))
-
+ 
 app.listen(port, () => {
   console.log(`Working! here is the port: ${port}`)
 })  
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const router = express.Router();
+//initializes firestore
+const firestore = require ("firebase/firestore");
+//  Create a reference to the database
+const db = firestore.getFirestore();
+
 
 router.use((req, res, next) => {
     console.log('Time: ', Date.now())
-    next()
+    next();
 });
 
 router.get('/', (req, res) => {
-    res.send('Hello Worlddddd (homepage)')
+    const postsQuery =  firestore.getDocs(firestore.collection(db, "posts"));
+    const postsArray = [];
+
+    postsQuery
+        .then((response) => {
+            response.forEach((post)=> {
+                console.log(post.data());
+                postsArray.push({id: post.id, ...post.data})
+                res.send(postsArray);
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.send(error);
+        });
+
+    // res.send('Hello Worlddddd (homepage)')
 });
 
 
 module.exports = router
+
+   //     querySnapshot.forEach((posts) => {
+    //         console.log(`${posts.id} => ${posts.data()}`);
+    // });
